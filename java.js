08e3d99a0ad2ab8b143ac01b8d49dd6785a0fe41
mansavy <100310641+mansavy@users.noreply.github.com>
2022-03-22 19:42:54 -0300

    Added javascript for real data for main cities

--- java.js
+++ java.js
@@ -165,7 +165,7 @@ function displayForecast(response) {
   let forecastElement = document.querySelector(".future-temps");
   let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
-    if (index < 5) {
+    if (index < 6 && index > 0) {
       forecastHTML =
         forecastHTML +
         ` <div class="col">
@@ -200,6 +200,50 @@ function getForecast(coordinates) {
   let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${key}&units=metric`;
   axios.get(url).then(displayForecast);
 }
+
+function clickNY(event) {
+  event.preventDefault();
+  let city = "New York";
+  let key = "bd72cea685abd0c8d8f5b8f11becd620";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
+  axios.get(url).then(displayWeather);
+}
+
+function clickLondon(event) {
+  event.preventDefault();
+  let city = "London";
+  let key = "bd72cea685abd0c8d8f5b8f11becd620";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
+  axios.get(url).then(displayWeather);
+}
+
+function clickParis(event) {
+  event.preventDefault();
+  let city = "Paris";
+  let key = "bd72cea685abd0c8d8f5b8f11becd620";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
+  axios.get(url).then(displayWeather);
+}
+
+function clickMunich(event) {
+  event.preventDefault();
+  let city = "Munich";
+  let key = "bd72cea685abd0c8d8f5b8f11becd620";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
+  axios.get(url).then(displayWeather);
+}
+
+let munich = document.querySelector("#munich");
+munich.addEventListener("click", clickMunich);
+
+let paris = document.querySelector("#paris");
+paris.addEventListener("click", clickParis);
+
+let london = document.querySelector("#london");
+london.addEventListener("click", clickLondon);
+
+let nyc = document.querySelector("#newYork");
+nyc.addEventListener("click", clickNY);
 
 let form = document.querySelector("#city-search");
 form.addEventListener("submit", handleSubmit);
